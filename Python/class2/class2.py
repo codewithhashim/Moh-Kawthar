74098e96932ada4fafa985a25759be24672d143b
prohashim <prohashim4e@gmail.com>
2022-09-25 22:50:10 +0500

    second python class

--- Python/class2/class2.py
+++ Python/class2/class2.py
@@ -1,3 +1,14 @@
 # named memory location
+# age in here is identifier 
+# = in here is assignment operator
+# Value of age is 2
 age = 2
 print(age)
+
+# names = input("hey name? ")
+
+# data types 
+data_n = 45345346
+data_s = "muhammad 12341 hahdaf"
+print(type(data_n))
+
