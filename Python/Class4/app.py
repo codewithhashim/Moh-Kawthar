c60aa12fbb1c712c205f31e2b68cd8fb36dc04ea
prohashim <prohashim4e@gmail.com>
2022-10-09 23:23:47 +0500

    class 5  ends

---
+++ Python/Class4/app.py
@@ -0,0 +1,85 @@
+# clicked_on_the_bar = "hmmm"
+# if clicked_on_the_bar == True:
+#     print("playing")
+# elif clicked_on_the_bar == False:
+#     print("Pause")
+# else:
+#     print("Nothing bruh")1
+
+# Weather => cloudy, rainy, sunny, stormy, foggy, hot, cold, windy, room
+
+# weather = "cloudy"
+# weather = "hot"
+# if weather == "cloudy":
+#     print("please keep umbrella")
+# elif weather == "rainy":
+#     print("please open the umbrella")
+# elif weather == "sunny":
+#     print()
+# elif weather == "stormy":
+#     print()
+# elif weather == "foggy":
+#     print()
+# else:
+#     print("Do your own liking")
+
+
+# Comparison OPerator
+# == operator
+
+# -= 
+# name = "Moh"
+# if name != "Kaw":
+#     print("Kawthar")
+
+
+    # > or < operator
+
+# a = 45
+# b = 455
+# if a < b:
+#     print("3435234")
+
+
+# LOGICAL operator 
+# and, or, not
+
+# and
+# from ast import Not
+
+
+# moh = True 
+# kaw = True
+
+# if moh == True and kaw == False:
+#     print("Open the door")
+
+
+# if moh == True or kaw == False:
+#     print("pstttt open")
+
+# if not(moh == True and kaw == False):
+#     print("close")
+
+
+
+# age = int(input("Your age? "))
+# match age:
+#     case 18:
+#         if age > 18:
+#             print("something")
+#         print("Not allowed in the children park")
+#     case 10:
+#         print("Allowed")
+#     case 5:
+#         print("Go HOME KID!")
+#     case _:
+#         print("Well no idea")
+
+
+
+# List
+arr = [45, 34, 55, 66, 77]
+
+
+weather = ["Windy", "hot", "cold", "sunny", "rainy"]
