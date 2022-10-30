394ec0bb5e50ef90c26c27309d5977cb03cb9898
prohashim <prohashim4e@gmail.com>
2022-10-30 22:49:58 +0500

    class 6 for Py

--- Python/class5/app.py
+++ Python/class5/app.py
@@ -1,11 +1,36 @@
 # if statements
 # conditionals
-if 1>0:
-    print("Hi")
 
-if 3>1:
-    print("Hello")
-elif 3< 1:
-    print()
-else:
-    print("hey") 
+# if 1>0:
+#     print("Hi")
+
+# if userHitthebutton == True:
+#     print("")
+
+# if 3>1:
+#     print("Hello")
+# elif 3< 1:
+#     print()
+# else:
+#     print("hey") 
+
+# concatenation
+
+# age = input("Your age: ")
+# person_name = input ("Your name: ")
+# print("Your name is " + person_name + "and your age is: " + age )
+
+from ast import If
+
+
+age = 34
+
+if  age>25:
+    print ("your old")
+elif age<25:
+    print ("your young")
+elif  age==25:
+    print ("your the one")
+else:   
+    print ("your nothing ")
+
