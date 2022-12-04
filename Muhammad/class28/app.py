a68e81ecf8c0a24ac1a57ddf51c8dca6facc7650
prohashim <prohashim4e@gmail.com>
2022-12-04 22:24:57 +0500

    class 28

---
+++ Muhammad/class28/app.py
@@ -0,0 +1,25 @@
+# string
+student_name = "Muhammad"
+
+# number
+age = 45
+
+# float
+height = 2.1
+
+# boolean 
+is_good = False
+
+# null (none)
+does_m_brain = None
+
+# if statement
+
+ages = 34
+if ages == 34:
+    print("The age is ok")
+  
+
+  agesy = 1000
+if  agesy == 1000 :
+    print ("you are master ogway")
