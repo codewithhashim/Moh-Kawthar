3fcf24bed27dd26da4a2ee6865cae1a5db9da22f
prohashim <prohashim4e@gmail.com>
2023-01-06 01:36:14 +0500

    before class 39

--- Kawthar/class37/app.py
+++ Kawthar/class37/app.py
@@ -1,4 +1,47 @@
 # variable 
 # = operator (assignment)
 # 45 is a value
+# int - integer (numeric)
 a = 45
+# string
+bn = "hello"
+# boolean
+is_cool = True
+# flaot
+# line 12 we are converting a float into integer
+# percent = int(56.6) int() flaot() str()
+percent = 45.10
+# int(percent)
+print(type(percent))
+print(percent)
+
+def classes():
+    somevar = 566
+
+# scope 
+def names():
+    print(percent)
+    # print(somevar)
+    # local scope
+    pirhana = "fish"
+    print (type("hello"))
+    print(pirhana)
+
+names()
+# print(pirhana)
+
+# if (1==1):
+#     complexz = 45
+
+
+# print(complexz)
+
+arr = [34]
+i = 0
+for i in arr:
+    zz = 456
+    def fun():
+        print()
+    print(i)
+
+print(zz)
