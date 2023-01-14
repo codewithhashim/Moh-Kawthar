ce449f72c0736846cccd4fc1c0d03ed9a77c3050
prohashim <prohashim4e@gmail.com>
2023-01-14 22:36:21 +0500

    end of class 41

---
+++ Kawthar/class39/app.py
@@ -0,0 +1,58 @@
+# data types
+
+# a placeholder for some kind of data
+st_name = "Kawthar"
+
+# why do we sneeze only when we are awake, and we do not sneeze while sleeping, any reason?
+# int 
+st_num = 34
+
+# true / false
+# boolean 
+sunset_east = False
+
+# witty (smarter)
+
+# properties ()
+
+
+
+# Functions ()
+
+
+# classes / object
+
+
+
+# Class 
+    # custom data-types (user-defined data-type)
+    # it will be blueprint for objects
+# Object
+    #properties
+    # methods
+
+class Person:
+    name = "kawthar"
+
+# creating an object. p0 is a object
+p0 = Person()
+p1 = Person()
+
+print(p0.name)
+print(p1.name)
+
+class location:
+    location ="123" 
+
+
+class Masjid:
+    name = "masjid"
+
+vis = Masjid()
+
+
+
+
+
+
+
---
+++ Kawthar/class41/app.py
@@ -0,0 +1,47 @@
+# class
+
+# object
+
+
+class MyClassDemo:
+    xyz = "demo"
+
+obj = MyClassDemo()
+
+# string
+studnetname = "kaw"
+st_age = 45
+
+
+# custom data type
+class Student:
+    name = ''
+    age = 12
+    print(age)
+
+# Student 1
+kaw = Student()
+kaws = Student()
+kawe = Student()
+kawr = Student()
+kawt = Student()
+
+
+class Plant:
+    type = ''
+    size ='max'
+    print (size)
+
+
+flower = Plant()
+mint = Plant()
+mango = Plant()
+
+
+class Notebook:
+    neutral_colors = ''
+    number_of_pages =  100
+    print(number_of_pages)
+
+english_notebook = Notebook()
+    
